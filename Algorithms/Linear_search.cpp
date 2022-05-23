68cd4c54e58cc6a0cbfb53625de16e8b2231ea1d
Eugene <69599396+Eugene-beginning-of-the-path@users.noreply.github.com>
2022-05-23 23:59:21 +0300

    Merged developer to master

--- Algorithms/Linear_search.cpp
+++ Algorithms/Linear_search.cpp
@@ -1,12 +1,32 @@
 #include <iostream>
 
+//Big O = (N)
+int linear_search(int arr[], const int size, int key)
+{
+	for (int i = 0; i < size; i++)
+	{
+		if (key == arr[i])
+			return i;
+	}
+	return -1;
+}
+
+static void showArr(int arr[], const int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		std::cout << arr[i] << "\t";
+	}
+	std::cout << std::endl;
+}
+
 void start_linear_search()
 {
 	const int size = 15;
 	int arr[size];
 	int key; //ключ поиска
 
-	for (size_t i = 0; i < size; i++)
+	for (int i = 0; i < size; i++) //не обязательно в линейном поиске массив должен быть отсортирован
 	{
 		arr[i] = i + 1;
 	}
@@ -18,24 +38,6 @@ void start_linear_search()
 	if (0 <= index)
 		std::cout << "Array has " << key << "; index = " << index << std::endl;
 	else
-		std::cout << "Array does not have " << key << std::endl;
-}
-
-int linear_search(int arr[], const int size, int key)
-{
-	for (size_t i = 0; i < size; i++)
-	{
-		if (key == arr[i])
-			return i;
-	}
-	return -1;
-}
-
-void showArr(int arr[], const int size)
-{
-	for (size_t i = 0; i < size; i++)
-	{
-		std::cout << arr[i] << "\t";
-	}
-	std::cout << std::endl;
+		if (index == -1)
+			std::cout << "Array does not have " << key << std::endl;
 }
